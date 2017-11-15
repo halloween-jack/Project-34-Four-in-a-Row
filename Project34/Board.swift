6c676043657471fb530d2573d72a5f51853acec3
halloween-jack <halloween.jack.dev@gmail.com>
2017-11-16 00:38:29 +0900

    Implementing GKGameModel: gameModelUpdates(for:) and apply()

--- Project34/Board.swift
+++ Project34/Board.swift
@@ -15,12 +15,19 @@ enum ChipColor: Int {
     case black
 }
 
-class Board: NSObject {
+class Board: NSObject, GKGameModel {
     static var width = 7
     static var height = 6
     
     var slots = [ChipColor]()
     var currentPlayer: Player
+    
+    var players: [GKGameModelPlayer]? {
+        return Player.allPlayers
+    }
+    var activePlayer: GKGameModelPlayer? {
+        return currentPlayer
+    }
     
     override init() {
         currentPlayer = Player.allPlayers[0]
@@ -104,5 +111,62 @@ class Board: NSObject {
         if chip(inColumn: col + (moveX * 3), row: row + (moveY * 3)) != initialChip { return false }
         
         return true
+    }
+    
+    func copy(with zone: NSZone? = nil) -> Any {
+        let copy = Board()
+        copy.setGameModel(self)
+        return copy
+    }
+    
+    func setGameModel(_ gameModel: GKGameModel) {
+        if let board = gameModel as? Board {
+            slots = board.slots
+            currentPlayer = board.currentPlayer
+        }
+    }
+    
+    func gameModelUpdates(for player: GKGameModelPlayer) -> [GKGameModelUpdate]? {
+        // 1
+        if let playerObject = player as? Player {
+            // 2
+            if isWin(for: playerObject) || isWin(for: playerObject.opponent) {
+                return nil
+            }
+            
+            // 3
+            var moves = [Move]()
+            
+            // 4
+            for column in 0 ..< Board.width {
+                if canMove(in: column) {
+                    // 5
+                    moves.append(Move(column: column))
+                }
+            }
+            
+            return moves
+        }
+        
+        return nil
+    }
+    
+    func apply(_ gameModelUpdate: GKGameModelUpdate) {
+        if let move = gameModelUpdate as? Move {
+            add(chip: currentPlayer.chip, in: move.column)
+            currentPlayer = currentPlayer.opponent
+        }
+    }
+    
+    func score(for player: GKGameModelPlayer) -> Int {
+        if let playerObject = player as? Player {
+            if isWin(for: playerObject) {
+                return 1000
+            } else if isWin(for: playerObject.opponent) {
+                return -1000
+            }
+        }
+        
+        return 0
     }
 }
